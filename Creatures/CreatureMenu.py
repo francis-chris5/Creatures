7827dd827c495aa813a51091a5b2eb3f9ca467f8
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2020-05-31 23:32:26 -0400

    Add files via upload

---
+++ Creatures/CreatureMenu.py
@@ -0,0 +1,50 @@
+bl_info = {
+    "name": "Add Creatures Menu",
+    "author": "Christopher S. Francis",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "",
+    "description": "Menu to add new <Creature> Mesh",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Add Mesh",
+}
+
+
+import bpy
+
+
+class CreatureMenu(bpy.types.Menu):
+    bl_label = "Creature Menu"
+    bl_idname = "object.Creatures"
+
+    def draw(self, context):
+        layout = self.layout
+        
+        layout.operator("mesh.add_box_turtle")
+        layout.operator("mesh.add_wolf")
+        layout.operator("mesh.add_tiger")
+        layout.operator("mesh.add_human")
+        
+        
+
+def draw_item(self, context):
+    layout = self.layout
+    layout.menu(CreatureMenu.bl_idname)
+
+
+def register():
+    bpy.utils.register_class(CreatureMenu)
+
+
+
+def unregister():
+    bpy.utils.unregister_class(CreatureMenu)
+
+
+
+if __name__ == "__main__":
+    register()
+
+    # The menu can also be called from scripts
+    bpy.ops.wm.call_menu(name=CreatureMenu.bl_idname)
